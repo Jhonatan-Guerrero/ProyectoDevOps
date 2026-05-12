92393f9f4005de7a8d78bd62637008b0630e18f3
adan ballesillo velazquez <adan_@BALLESILLO1.COMADREJA2.local>
2026-05-11 20:07:12 -0600

    modificacion de migraciones

--- comadreja-shop/src/database/migrations/2026_05_11_193421_create_cart_items_table.php
+++ comadreja-shop/src/database/migrations/2026_05_11_193421_create_cart_items_table.php
@@ -6,22 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('cart_items', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('cart_items', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('cart_items');
+        Schema::table('cart_items', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
     }
 };
--- comadreja-shop/src/database/migrations/2026_05_11_193421_create_carts_table.php
+++ comadreja-shop/src/database/migrations/2026_05_11_193421_create_carts_table.php
@@ -6,22 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('carts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('carts', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('carts');
+        Schema::table('carts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
     }
 };
